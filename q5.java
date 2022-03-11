cf18a6fee5a4f91d6a61c4adfc0a9a63c0064335
mrunalizope <100849607+mrunalizope@users.noreply.github.com>
2022-03-11 22:10:12 +0530

    Add files via upload

---
+++ q5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class q5
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println("Given Number is Positive");
+}
+else
+{
+System.out.println("Given Number is Negative");
+}
+}
+}
